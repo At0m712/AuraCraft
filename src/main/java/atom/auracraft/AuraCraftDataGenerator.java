ac11979175b9bf704513447404acb87ad7136979
Mathis ALLEWEIRELDT <mathis.alleweireldt.etu@univ-lille.fr>
2026-09-23 22:12:45 +0200

    Init.

---
+++ src/main/java/atom/auracraft/AuraCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package atom.auracraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AuraCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
